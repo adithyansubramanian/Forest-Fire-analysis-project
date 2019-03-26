c0dafab5261927314295cbe0290718a659fe4057
Adithyan Subramanian <adithyan.subramanian19@gmail.com>
2019-03-26 13:10:30 -0500

    Add files via upload

---
+++ Forest fires project.R
@@ -0,0 +1,15 @@
+fires <- read.csv("forestfires.csv")
+#analyze which month had the most forest fires
+#load dplyr and ggplot
+#library("dplyr")
+#library("ggplot2")
+#group by month
+#review_month <- fires %>%
+#  group_by(month) %>%
+#  summarize(total = n())
+
+
+#ggplot(data = review_month) + 
+#  aes(x = month, y = total) +
+#  geom_bar(stat = "identity")
+
